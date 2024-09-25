a115e91ad342b1f1c055f8f33e5af729a01ee9c6
Spear-woo <chwok33@gmail.com>
2024-09-25 18:04:01 +0900

    Add files via upload

---
+++ 02.환경설정.py
@@ -0,0 +1,9 @@
+print("Hello Python!")
+
+# upper, lower, replace, find, count, split, join, lstrip, rstip, strip
+
+test = ['123', 'teststest', '12313131', 'sffaa', 'fzzz']
+
+# join >> 문자열만 가능. 숫자는 안 된다.
+a = ':'.join(test)
+print(a)
